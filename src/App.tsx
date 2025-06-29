5e312ca77d167fdf1759895b9e932b26456ae050
Erwin Dela Cruz <76925531+decrefy-developer@users.noreply.github.com>
2025-06-29 12:42:31 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react'
+import ElodiePng from './assets/Elodie.jpeg'
 import './App.css'
 
 function App() {
@@ -8,7 +9,15 @@ function App() {
 
   return (
     <>
-      <h1>Elodi Hope Dela Cruz</h1>
+      <div>
+        <a href="# target="_blank">
+          <img src={ElodiePng} className="logo" alt="Elodie logo" />
+        </a>
+      </div>
+      <h1>Elodie Hope Dela Cruz</h1>
+      // <p className="read-the-docs">
+      //   Click on the Vite and React logos to learn more
+      // </p>
     </>
   )
 }
